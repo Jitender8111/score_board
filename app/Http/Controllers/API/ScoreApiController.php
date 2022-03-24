67584ca971fcfa55220282a6e3b1a9fd3c61af37
jitendertrigvent <jitenderkashyap@trigvent.com>
2022-03-24 21:48:56 +0530

    small changes done

--- app/Http/Controllers/API/ScoreApiController.php
+++ app/Http/Controllers/API/ScoreApiController.php
@@ -15,7 +15,7 @@ class ScoreApiController extends Controller
     public function getScore(){
      \DB::statement("SET SQL_MODE=''");  // solve access voilation error if you got it, by this query
    
-    $scores = User::leftJoin('score', 'score.user_id', '=', 'users.id')->groupBy('score.user_id')->select('users.*','score.user_id', DB::raw('sum(score.score) as total_score'))->orderBy('total_score')->get();
+    $scores = User::leftJoin('score', 'score.user_id', '=', 'users.id')->groupBy('score.user_id')->select('users.*','score.user_id','score.id', DB::raw('sum(score.score) as total_score'))->orderBy('total_score')->get();
   
      // now set all records in array using according to format 
      $leader_board =[];
@@ -26,9 +26,10 @@ class ScoreApiController extends Controller
         $data['created_at'] =$score->created_at;
         $data['updated_at'] =$score->updated_at;
         $data['total_score'] =$score->total_score;
-        $data['user'] = array('id' => $score->id, 'name'=>$score->name);
+        $data['user'] = array('id' => $score->user_id, 'name'=>$score->name);
         $leader_board[]=$data;
      }
+     
         return response()->json(['success'=>true,"leaderboard" => $leader_board]); 
     }
 }
